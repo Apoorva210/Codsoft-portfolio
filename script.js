5d231496bf55a53b9fb699af6aa756346567376d
Apoorva210 <96936309+Apoorva210@users.noreply.github.com>
2024-03-11 12:41:24 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('#element', {
+        strings: ['Web Developer.','Web Designer.'],
+        typeSpeed: 50,
+    });
+});
